2f3d76f076c1d05053d906f1c6e54b82eb5e1851
Ryan <ryangohjq@gmail.com>
2019-11-06 15:19:00 +0800

    Fixed issue where create_log_pusher_role is false and base_overlay json cannot be computed

--- main.tf
+++ main.tf
@@ -523,7 +523,7 @@ data "aws_iam_policy_document" "es_vpc_management_access_base_overlay" {
 
     principals {
       type        = "AWS"
-      identifiers = distinct(compact(flatten([var.log_pusher_iam_roles, aws_iam_role.log_pusher[0].arn])))
+      identifiers = distinct(compact(flatten([var.log_pusher_iam_roles, var.create_log_pusher_role ? aws_iam_role.log_pusher[0].arn : []])))
     }
   }
 }
